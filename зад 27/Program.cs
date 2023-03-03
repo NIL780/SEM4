a540ec84641cf0fa4c19c920e4b9388b47cdf725
NATA <armida780@mail.ru>
2023-03-03 13:38:49 +0300

    Commit2

--- зад 27/Program.cs
+++ зад 27/Program.cs
@@ -1,11 +1,20 @@
-﻿Console.Clear();
+﻿// Зад.25
+// Напиcать цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Clear();
 int numA= ReadLine ("Введите число A: ");
 int numB= ReadLine("Введите число B: ");
+int ReadLine (string message)
+{Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());}
+
+
 double n=numA;
 double power=numB;
 int result= Convert.ToInt32(Math.Pow(n,power));
 {Console.WriteLine(result);}
 
-int ReadLine (string message)
-{Console.WriteLine(message);
-return Convert.ToInt32(Console.ReadLine());}
+// int ReadLine (string message)
+// {Console.WriteLine(message);
+// return Convert.ToInt32(Console.ReadLine());}
