6cc8019bed0ab30662775ce2dff982d4853bcac9
NATA <armida780@mail.ru>
2023-03-03 13:26:32 +0300

    два число_степень

--- зад 27/Program.cs
+++ зад 27/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int numA= ReadLine ("Введите число A: ");
+int numB= ReadLine("Введите число B: ");
+double n=numA;
+double power=numB;
+int result= Convert.ToInt32(Math.Pow(n,power));
+{Console.WriteLine(result);}
+
+int ReadLine (string message)
+{Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());}
